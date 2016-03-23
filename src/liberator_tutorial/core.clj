9cf460341f319f09d31884e6d4cb8abad3c65f40
pikariop <pikariop@gmail.com>
2016-03-23 19:30:58 +0200

    Initial commit

---
+++ src/liberator_tutorial/core.clj
@@ -0,0 +1,13 @@
+(ns liberator-tutorial.core
+  (:require [liberator.core :refer [resource defresource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes ANY]]))
+
+(defroutes app
+  (ANY "/foo" [] (resource :available-media-types ["text/html"]
+                           :handle-ok (fn [ctx]
+                                        (format "<html>It's %d milliseconds since the beginning of the epoch."
+                                                (System/currentTimeMillis))))))
+(def handler
+  (-> app
+      wrap-params))
